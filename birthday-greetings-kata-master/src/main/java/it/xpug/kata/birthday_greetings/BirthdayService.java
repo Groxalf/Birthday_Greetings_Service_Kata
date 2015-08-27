bc8e8bcfc07e873134155834f102a86ffe9121ea
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-08-28 00:07:56 +0100

    extract employees repository

--- birthday-greetings-kata-master/src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
+++ birthday-greetings-kata-master/src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -14,7 +16,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class BirthdayService {
-    private final MailService mailService;
+    protected EmployeeRepository employeeRepository;
+    protected MailService mailService;
     private String employees;
 
     public BirthdayService(String employees, MailService mailService) {
@@ -22,20 +25,39 @@ public class BirthdayService {
         this.mailService = mailService;
     }
 
-    public void sendGreetings(XDate xDate) throws IOException, ParseException, AddressException, MessagingException {
+    public BirthdayService(EmployeeRepository employeeRepository, MailService mailService) {
+        this.employeeRepository = employeeRepository;
+        this.mailService = mailService;
+    }
+
+    protected void sendGreetings(final XDate xDate) throws IOException, ParseException, AddressException, MessagingException {
+        for (Employee employee : getEmployees()) {
+            xxx(xDate, employee);
+        }
+    }
+
+    protected List<Employee> getEmployees() throws IOException, ParseException {
         BufferedReader in = new BufferedReader(new FileReader(employees));
         String str = "";
         str = in.readLine(); // skip header
+        List<Employee> employeesRepository = new ArrayList<Employee>();
         while ((str = in.readLine()) != null) {
             String[] employeeData = str.split(", ");
             Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
-            if (employee.isBirthday(xDate)) {
-                String recipient = employee.getEmail();
-                String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
-                String subject = "Happy Birthday!";
-                mailService.sendMessage("sender@here.com", subject, body, recipient);
-            }
-        }
+            employeesRepository.add(employee);
 
+        }
+        return employeesRepository;
     }
+
+    private void xxx(XDate xDate, Employee employee) throws MessagingException {
+        if (employee.isBirthday(xDate)) {
+            String recipient = employee.getEmail();
+            String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
+            String subject = "Happy Birthday!";
+            mailService.sendMessage("sender@here.com", subject, body, recipient);
+        }
+    }
+
+
 }
