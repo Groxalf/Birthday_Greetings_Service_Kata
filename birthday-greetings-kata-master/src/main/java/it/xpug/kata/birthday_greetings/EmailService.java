b25d0c4d80d6b0791a26e4391fa544090af92377
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-09-01 21:49:08 +0100

    renames

--- birthday-greetings-kata-master/src/main/java/it/xpug/kata/birthday_greetings/EmailService.java
+++ birthday-greetings-kata-master/src/main/java/it/xpug/kata/birthday_greetings/EmailService.java
@@ -27,10 +27,6 @@ public class EmailService {
 
     public void sendEmail(String senderEmail, String subject, String body, String recipient) throws AddressException, MessagingException {
         // Create a mail session
-        java.util.Properties props = new java.util.Properties();
-        props.put("mail.smtp.host", hostname);
-        props.put("mail.smtp.port", "" + port);
-        Session session = Session.getInstance(props, null);
         java.util.Properties properties = new java.util.Properties();
         properties.put("mail.smtp.host", hostname);
         properties.put("mail.smtp.port", String.valueOf(port));
